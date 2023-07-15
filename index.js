cd2fea1e666fc6ab4163f2e207266a3f0b6cede5
Gabe Corsini <gabe.corsini@gmail.com>
2023-07-14 23:17:31 -0400

    Fix logic for game loop
    There's a bug where every time the Computer's score increments by one
    regardless if the Computer actually won or not. There's an issue
    somewhere with the logic. I need to find that.

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 
-
-function getComputerChoice(choices){
+function getComputerChoice(choices) {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getPlayerChoice(choices){
+function getPlayerChoice(choices) {
     let playerChoice = prompt("Pick Rock, Paper, or Scissors!");
-    if(playerChoice.toLowerCase == "rock"){
+    if (playerChoice.toLowerCase() == "rock") {
         return choices[0];
     }
-    else if(playerChoice.toLowerCase == "paper"){
+    else if (playerChoice.toLowerCase() == "paper") {
         return choices[1];
     }
-    else if(playerChoice.toLowerCase == "scissors"){
+    else if (playerChoice.toLowerCase() == "scissors") {
         return choices[2];
     }
     else {
@@ -21,17 +20,57 @@ function getPlayerChoice(choices){
     }
 }
 
-function winCondition(computerChoice, playerChoice){
-
+function winCondition(computerChoice, playerChoice) {
+    if (computerChoice == "rock" && playerChoice == "scissors") {
+        return "You lose!";
+    } else if (computerChoice == "rock" && playerChoice == "paper") {
+        return "You win!";
+    } else if (computerChoice == "rock" && playerChoice == "rock") {
+        return "You tie!";
+    } else if (computerChoice == "paper" && playerChoice == "rock") {
+        return "You lose!";
+    } else if (computerChoice == "paper" && playerChoice == "scissors") {
+        return "You win!";
+    } else if (computerChoice == "paper" && playerChoice == "paper") {
+        return "You tie!";
+    } else if (computerChoice == "scissors" && playerChoice == "paper") {
+        return "You lose!";
+    } else if (computerChoice == "scissors" && playerChoice == "rock") {
+        return "You win!";
+    } else if (computerChoice == "scissors" && playerChoice == "scissors") {
+        return "You tie!";
+    }
 }
 
-function gameLoop(){
+function gameLoop() {
     const choiceArray = ['Rock', 'Paper', 'Scissors'];
     console.log(`Your choices are ${choiceArray.join(", ")} - Pick one!`);
 
-    let computerChoice = getComputerChoice(choiceArray);
-    let playerChoice = getPlayerChoice(choiceArray);
+    let computerChoice = String(getComputerChoice(choiceArray)).toLowerCase();
+    let playerChoice = String(getPlayerChoice(choiceArray)).toLowerCase();
+
+    let winStatement = winCondition(computerChoice, playerChoice);
+
+
+    switch (winStatement) {
+        case 'You lose!':
+            return 'You lose!';
+        case 'You win!':
+            return 'You win!'
+        default:
+            return 'You tie!';
+    }
 }
 
+let gameLimit = 3;
+let scoreBoard = { "Computer": 0, "Player": 0 };
 
-gameLoop();
+while (scoreBoard["Computer"] < gameLimit || scoreBoard["Player"] < gameLimit) {
+    let result = gameLoop();
+    switch (result) {
+        case 'You win!':
+            scoreBoard["Player"] += 1;
+        case 'You lose!':
+            scoreBoard["Computer"] += 1;
+    }
+}
